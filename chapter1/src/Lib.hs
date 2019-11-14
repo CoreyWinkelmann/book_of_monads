a982800a462529fe7488f980638e5aa39c7ad8bf
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2019-11-13 22:43:16 -0700

    section 1.1

--- chapter1/src/Lib.hs
+++ chapter1/src/Lib.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE TupleSections #-}
+
 module Lib where
 
-data Tree a = Leaf a | Node (Tree a) (Tree a)
+import Prelude hiding (pure)
+
+-- section 1.1
+
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Show, Eq)
 
 numberOfLeaves :: Tree a -> Integer
 numberOfLeaves (Leaf _) = 1
@@ -11,3 +17,26 @@ relabel (Leaf x) i = (Leaf (i, x), i + 1)
 relabel (Node l r) i = let (l', i1) = relabel l i
                            (r', i2) = relabel r i1
                         in (Node l' r', i2)
+
+-- I am doing a stack ghci of this repo
+-- let t = Node (Node (Leaf 1) (Node (Leaf 2) (Leaf 3))) (Leaf 4)
+-- relabel t
+-- numberOfLeaves t
+
+type WithCounter a = Integer -> (a, Integer)
+
+-- I believe this has a similar shape to monad's bind function
+next :: WithCounter a -> (a -> WithCounter b) -> WithCounter b
+f `next` g = \i -> let (r, i') = f i in g r i'
+
+pure :: a -> WithCounter a
+pure x = (x,)
+
+-- Redo relabel with the new WithCounter type
+relabel' :: Tree a -> WithCounter (Tree (Integer, a))
+relabel' (Leaf x) = \i -> (Leaf (i, x), i + 1)
+relabel' (Node l r) = relabel' l `next` \l' ->
+                      relabel' r `next` \r' ->
+                      pure (Node l' r')
+
+-- section 1.2
