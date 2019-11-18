63bb8ad6bd49e88a7a0f63d237e4e5260639810d
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2019-11-17 22:05:51 -0700

    chapter2 - do notation so minimal examples

--- chapter2/src/Lib.hs
+++ chapter2/src/Lib.hs
@@ -1,1 +1,10 @@
 module Lib where
+
+import Control.Monad.State.Lazy
+
+incrCounter :: State Integer Integer
+incrCounter = do n <- get
+                 p <- put (n + 1)
+                 return (n + 1)
+
+testingState = print $ evalState incrCounter 0
