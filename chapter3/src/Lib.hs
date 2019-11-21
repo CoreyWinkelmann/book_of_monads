5fcab6871643af20b5965b2b94d703787bccccba
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2019-11-20 22:56:12 -0700

    Chapter 3

--- chapter3/src/Lib.hs
+++ chapter3/src/Lib.hs
@@ -15,3 +15,20 @@ lift2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
 lift2 f x y = do a <- x
                  b <- y
                  return (f a b)
+
+-- Ex. 3.2
+fmap' :: Applicative f => (a -> b) -> f a -> f b
+fmap' f b = pure f <*> b
+
+newtype ZipList a = ZipList { getZipList :: [a] } deriving (Show)
+
+-- Ex. 3.3
+instance Functor ZipList where
+  -- fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap f zxs = ZipList $ map f $ getZipList zxs
+
+instance Applicative ZipList where
+  pure x = ZipList (repeat x)
+  ZipList fs <*> ZipList xs = ZipList (zipWith (\f x -> f x) fs xs)
+
+
